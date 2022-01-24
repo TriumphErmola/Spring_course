502ac1c19e97dfe6c01ad777a1011121738688b0
Andrey <xramovic@yandex.ru>
2022-01-24 19:44:49 +0600

    init/destroy

--- src/main/java/spring_introduction/Test5.java
+++ src/main/java/spring_introduction/Test5.java
@@ -8,6 +8,10 @@ public class Test5 {
                 new ClassPathXmlApplicationContext("applicationContext2.xml");
         Dog myDog = context.getBean("myPet", Dog.class);
         myDog.say();
+
+        Dog yourDog = context.getBean("myPet", Dog.class);
+        yourDog.say();
+
         context.close();
     }
 }
