549ce40e1ebcec626e91485d92444dcf312a8b85
Andrey <xramovic@yandex.ru>
2022-01-29 13:39:07 +0600

    Poincat

--- src/main/java/aop/SchoolLibrary.java
+++ src/main/java/aop/SchoolLibrary.java
@@ -10,5 +10,6 @@ public class SchoolLibrary extends AbstractLibrary {
     public void getBook() {
         System.out.println("Мы берём книгу из SchoolLibrary");
 
+
     }
 }
--- src/main/java/aop/UniLibrary.java
+++ src/main/java/aop/UniLibrary.java
@@ -10,12 +10,26 @@ public class UniLibrary extends AbstractLibrary {
         System.out.println("Мы берём книгу из UniLibrary : ");
     }
 
-//    public String returnBook() {
-//        System.out.println("Возвращаем книгу в UniLibrary");
-//        return "ОК";
-//    }
+    public void returnBook() {
+        System.out.println("Возвращаем книгу в UniLibrary");
+
+    }
 
     public void getMagazine() {
         System.out.println("Мы берём журнал из UniLibrary");
     }
+
+    public void returnMagazine() {
+        System.out.println("Возвращаем журнал в UniLibrary");
+    }
+
+    public void addBook() {
+        System.out.println("Мы добавляем книгу в UniLibrary");
+    }
+
+    public void addMagazine() {
+        System.out.println("Мы добавляем журнал в UniLibrary");
+    }
+
+
 }
